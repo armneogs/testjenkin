5d25e6f157feba5b3c999dc8cd64beb74148f991
Krist Kochsuwan <krist.k@cdg.co.th>
2020-01-14 09:04:26 +0700

    egrf

---
+++ src/main/java/com/army/TestJenkinApplication.java
@@ -0,0 +1,13 @@
+package com.army;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJenkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestJenkinApplication.class, args);
+	}
+
+}
